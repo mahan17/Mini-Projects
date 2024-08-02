90701e18d9d81a571f29f4a2ffd9bde690adfe80
Mahan <mahankaliyamurthy@gmail.com>
2024-08-02 13:13:52 +0530

    Delete login directory

--- login/practice/input.js
+++
@@ -1,9 +0,0 @@
-function fullName(event){
-    let fname = document.getElementsByName('fnm')[0].value
-
-    let lname = document.getElementsByName('lnm')[0].value
-    let fulName = document.getElementsByClassName('result')[0]
-
-    fulName.innerText = `Full Name is ${fname} ${lname}`
-    event.preventDefault()
-}
